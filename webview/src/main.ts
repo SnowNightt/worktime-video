47a36479f5e1e7b6953a9ba1356c9bdba7e64659
SnowNightt <1006817009@qq.com>
2026-05-02 16:55:56 +0800

    style: 将全局样式入口由css改为scss

--- webview/src/main.ts
+++ webview/src/main.ts
@@ -1,8 +1,8 @@
-import { createApp } from 'vue';
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
-import App from './App.vue';
-import './styles/base.css';
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import App from "./App.vue";
+import "./styles/base.scss";
 const app = createApp(App);
 app.use(ElementPlus);
-app.mount('#app');
+app.mount("#app");
