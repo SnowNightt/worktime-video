92311f75f4500c605a144a5f339638a074eb5fda
SnowNightt <1006817009@qq.com>
2026-05-31 15:01:48 +0800

    feat(music): 播放器增加进度条

--- webview/src/features/cloude-music/components/audioBar.vue
+++ webview/src/features/cloude-music/components/audioBar.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="audio-container">
-    <audio ref="audioRef" class="audio" />
+    <audio ref="audioRef" class="audio" @timeupdate="handleTimeUpdate" />
+    <div class="progress-bar">
+      <div class="progress-active" :style="{ width: progressPercent + '%' }"></div>
+    </div>
     <div class="prev controler" @click="handlePrevMusic">
       {{ "⏮" }}
     </div>
@@ -19,9 +22,12 @@ const {
   currentSong,
   currentUrl,
   isPlaying,
+  progressPercent,
+  currentTime,
   switchPlayingStatus,
   handlePrevMusic,
   handleNextMusic,
+  timeUpdate,
 } = useAudioPlayer();
 const audioRef = ref<HTMLAudioElement>();
 watch(currentUrl, newVal => {
@@ -29,6 +35,11 @@ watch(currentUrl, newVal => {
   audioRef.value.src = newVal;
   handleSwitchStatus();
 });
+// 当前播放时间发生变化
+const handleTimeUpdate = () => {
+  const currentTime = audioRef.value!.currentTime;
+  timeUpdate(currentTime);
+};
 // 播放/暂停
 const handleSwitchStatus = () => {
   switchPlayingStatus();
@@ -47,7 +58,8 @@ const handleSwitchStatus = () => {
   bottom: 15px;
   height: 35px;
   background-color: rgba(191, 191, 191, 0.5);
-  border-radius: 7px;
+  border-bottom-left-radius: 7px;
+  border-bottom-right-radius: 7px;
   backdrop-filter: blur(10px);
 
   .controler {
@@ -58,6 +70,21 @@ const handleSwitchStatus = () => {
   }
   .current-status {
     margin: 0 20px;
+  }
+  .progress-bar {
+    position: absolute;
+    width: 100%;
+    top: 0;
+    cursor: pointer;
+    background-color: #d9d9d9;
+    .progress-active {
+      background-color: #69c0ff;
+      height: 3px;
+    }
+  }
+  .progress-bar:hover .progress-active {
+    height: 6px;
+    // box-shadow: 2px 0 2px 4px rgba(140, 140, 140, 0.35);
   }
 }
 </style>
--- webview/src/features/cloude-music/hooks/useAudioPlayer.ts
+++ webview/src/features/cloude-music/hooks/useAudioPlayer.ts
@@ -1,24 +1,29 @@
 import { ref } from "vue";
 import { getMusicUrlApi } from "../api";
-import { Song } from "../type";
+import { MusicUrlItem, Song } from "../type";
 // 当前音乐url、名称
 const currentUrl = ref("");
-const currentSong = ref("");
+const currentSong = ref<MusicUrlItem>();
+// 当前播放的时间
+const currentTime = ref();
 // 播放状态
 const isPlaying = ref(false);
 // 当前歌曲所在的歌单
 const playingList = ref<Song[]>([]);
 // 当前播放的音乐在歌单里的索引
 const currentIndex = ref();
+// 进度条长度
+const progressPercent = ref("0");
 export const useAudioPlayer = () => {
   // 获取音乐url
   const getMusicUrl = async (id: number, musicList?: Song[]) => {
     if (musicList) {
       playingList.value = musicList;
     }
-    currentIndex.value = playingList.value.findIndex(item => item.id === id);
     const res = await getMusicUrlApi({ id, level: "higher" });
+    currentSong.value = res.data[0];
     currentUrl.value = res.data[0].url;
+    currentIndex.value = playingList.value.findIndex(item => item.id === id);
   };
 
   // 播放/暂停
@@ -26,24 +31,39 @@ export const useAudioPlayer = () => {
     isPlaying.value = !isPlaying.value;
   };
 
-  // todo: 加边界判断
+  // 边界判断
   // 上一首
   const handlePrevMusic = () => {
-    const id = playingList.value[--currentIndex.value].id;
+    const id =
+      playingList.value[
+        --currentIndex.value < 0 ? playingList.value.length - 1 : currentIndex.value
+      ].id;
     getMusicUrl(id);
   };
   // 下一首
   const handleNextMusic = () => {
-    const id = playingList.value[++currentIndex.value].id;
+    const id =
+      playingList.value[++currentIndex.value === playingList.value.length ? 0 : currentIndex.value]
+        .id;
     getMusicUrl(id);
+  };
+
+  // 当前播放时间发生变化
+  const timeUpdate = (time: number) => {
+    progressPercent.value = (((time * 1000) / currentSong.value!.time) * 100).toFixed(2);
+    currentTime.value = time;
+    console.log(1111, progressPercent.value);
   };
   return {
     currentSong,
     currentUrl,
     isPlaying,
+    progressPercent,
+    currentTime,
     getMusicUrl,
     switchPlayingStatus,
     handlePrevMusic,
     handleNextMusic,
+    timeUpdate,
   };
 };
